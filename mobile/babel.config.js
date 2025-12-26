0edc8f3d0ac7bf3b32cf030868f3acabf8b730f6
Nathan Doyle <nathandoyle@Nathans-MacBook-Pro.local>
2025-12-26 00:38:30 +0000

    Simplify Babel config - let babel-preset-expo handle Flow
    Remove redundant env.test config. The babel-preset-expo should
    automatically handle Flow syntax parsing. If issues persist, we may
    need to check jest-expo's Babel configuration.

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,14 +1,16 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      [
+        'babel-preset-expo',
+        {
+          // Explicitly enable all syntax features including Flow
+          jsxRuntime: 'automatic',
+        },
+      ],
+    ],
     plugins: ['@babel/plugin-transform-flow-strip-types'],
-    env: {
-      test: {
-        presets: ['babel-preset-expo'],
-        plugins: ['@babel/plugin-transform-flow-strip-types'],
-      },
-    },
   };
 };
 
