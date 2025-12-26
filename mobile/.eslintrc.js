75de6e9063d714bb651897336ea8650646c4518e
Nathan Doyle <nathandoyle@Nathans-MacBook-Pro.local>
2025-12-26 22:59:28 +0000

    Upgrade expo eslint config for TS ESLint v8

--- mobile/.eslintrc.js
+++ mobile/.eslintrc.js
@@ -6,6 +6,10 @@ module.exports = {
   plugins: ['prettier'],
   rules: {
     'prettier/prettier': 'error',
+    // v8 replacement rules for ban-types defaults
+    '@typescript-eslint/no-empty-object-type': 'error',
+    '@typescript-eslint/no-unsafe-function-type': 'error',
+    '@typescript-eslint/no-wrapper-object-types': 'error',
   },
   ignorePatterns: ['node_modules/', '.expo/', 'dist/'],
 };
