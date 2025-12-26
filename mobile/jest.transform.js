ae94620445fddae861ba2ca972e29200d816420f
Nathan Doyle <nathandoyle@Nathans-MacBook-Pro.local>
2025-12-26 00:51:16 +0000

    Route react-native/jest/setup.js through Flow transformer
    setup.js contains Flow syntax (number => void) that TypeScript parser
    cannot handle. Route it through Flow transformer, while other RN jest
    files (with 'as' syntax) go through TypeScript transformer.

--- mobile/jest.transform.js
+++ mobile/jest.transform.js
@@ -4,39 +4,36 @@ const expoTransformer = createTransformer({
   presets: ['babel-preset-expo'],
 });
 
-// Use Flow parser for react-native/jest files since they contain Flow syntax
-// that TypeScript parser can't handle (e.g., callback: number => void)
-const rnJestTransformer = createTransformer({
+const rnFlowTransformer = createTransformer({
   babelrc: false,
   configFile: false,
   presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
-  plugins: [
-    '@babel/plugin-transform-flow-strip-types',
-    ['@babel/plugin-transform-typescript', { allExtensions: true }],
-  ],
-  parserOpts: {
-    plugins: ['flow', 'flowComments'],
-  },
+  plugins: ['@babel/plugin-transform-flow-strip-types'],
+});
+
+const rnTsTransformer = createTransformer({
+  babelrc: false,
+  configFile: false,
+  presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
+  plugins: [['@babel/plugin-transform-typescript', { allExtensions: true }]],
 });
 
 function isReactNativeJestFile(filename) {
   return filename.includes('node_modules/react-native/jest/');
 }
 
+function isReactNativeJestSetup(filename) {
+  return filename.endsWith('node_modules/react-native/jest/setup.js');
+}
+
 module.exports = {
   process(src, filename, config, options) {
     if (isReactNativeJestFile(filename)) {
-      return rnJestTransformer.process(src, filename, config, options);
+      if (isReactNativeJestSetup(filename)) {
+        return rnFlowTransformer.process(src, filename, config, options);
+      }
+      return rnTsTransformer.process(src, filename, config, options);
     }
     return expoTransformer.process(src, filename, config, options);
-  },
-  getCacheKey(src, filename, configString, options) {
-    if (isReactNativeJestFile(filename) && rnJestTransformer.getCacheKey) {
-      return rnJestTransformer.getCacheKey(src, filename, configString, options);
-    }
-    if (expoTransformer.getCacheKey) {
-      return expoTransformer.getCacheKey(src, filename, configString, options);
-    }
-    return '';
   },
 };
