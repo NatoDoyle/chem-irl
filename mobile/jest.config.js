0b9e80a6fd5e9e285ddbfe5dae8991df68124d3d
Nathan Doyle <nathandoyle@Nathans-MacBook-Pro.local>
2025-12-26 01:34:06 +0000

    Simplify Jest config: remove custom transformer, use jest-expo preset
    The custom transformer was causing parse errors because it wasn't using
    the correct Babel config for React Native jest mocks. Simplified to use
    jest-expo preset which handles RN mocks correctly.
    
    - Removed jest.transform.js (custom transformer)
    - Simplified jest.config.js to use default jest-expo preset
    - Added jest config to package.json with fakeTimers
    
    Next: restore node_modules with clean install

--- mobile/jest.config.js
+++ mobile/jest.config.js
@@ -1,11 +1,5 @@
 module.exports = {
   preset: 'jest-expo',
   setupFiles: ['<rootDir>/jest.setup.js'],
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/jest.transform.js',
-  },
-  transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|@react-native-community|expo(nent)?|@expo(nent)?/.*|expo-modules-core|@unimodules|unimodules)/)',
-  ],
 };
 
